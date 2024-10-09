d4968a0d646c75b3352652b4fb4d4602dc5253e0
MgShepherd <michaelshep1504@gmail.com>
2024-10-09 20:51:38 +0100

    Added support for custom comparison functions

--- src/sort.zig
+++ src/sort.zig
@@ -1,22 +1,33 @@
 const std = @import("std");
 
 pub const SortType = enum { Bubble, Selection, Insertion };
+pub const Ordering = enum { Before, Equal, After };
+
+const CompareFunction = fn (item1: i32, item2: i32) Ordering;
 
 pub fn sort(elements: []i32, sortType: SortType) void {
+    processSort(elements, sortType, compareElements);
+}
+
+pub fn customSort(elements: []i32, sortType: SortType, comptime compareFn: CompareFunction) void {
+    processSort(elements, sortType, compareFn);
+}
+
+fn processSort(elements: []i32, sortType: SortType, comptime compareFn: CompareFunction) void {
     switch (sortType) {
-        .Bubble => bubbleSort(elements),
-        .Selection => selectionSort(elements),
-        .Insertion => insertionSort(elements),
+        .Bubble => bubbleSort(elements, compareFn),
+        .Selection => selectionSort(elements, compareFn),
+        .Insertion => insertionSort(elements, compareFn),
     }
 }
 
-fn bubbleSort(elements: []i32) void {
+fn bubbleSort(elements: []i32, comptime compareFn: CompareFunction) void {
     var i: usize = 0;
     while (i < elements.len - 1) : (i += 1) {
         var numSwaps: usize = 0;
         var j: usize = 0;
         while (j < elements.len - 1) : (j += 1) {
-            if (elements[j] > elements[j + 1]) {
+            if (compareFn(elements[j + 1], elements[j]) == Ordering.Before) {
                 numSwaps += 1;
                 swap(elements, j, j + 1);
             }
@@ -25,30 +36,39 @@ fn bubbleSort(elements: []i32) void {
     }
 }
 
-fn selectionSort(elements: []i32) void {
+fn selectionSort(elements: []i32, comptime compareFn: CompareFunction) void {
     var i: usize = 0;
     while (i < elements.len - 1) : (i += 1) {
         var j: usize = i + 1;
-        var minIndex: usize = i;
+        var swapIndex: usize = i;
         while (j < elements.len) : (j += 1) {
-            if (elements[j] < elements[minIndex]) {
-                minIndex = j;
+            if (compareFn(elements[j], elements[swapIndex]) == Ordering.Before) {
+                swapIndex = j;
             }
         }
-        if (minIndex != i) {
-            swap(elements, i, minIndex);
+        if (swapIndex != i) {
+            swap(elements, i, swapIndex);
         }
     }
 }
 
-fn insertionSort(elements: []i32) void {
+fn insertionSort(elements: []i32, comptime compareFn: CompareFunction) void {
     var i: usize = 1;
     while (i < elements.len) : (i += 1) {
         var j: usize = i;
-        while (j > 0 and elements[j] < elements[j - 1]) : (j -= 1) {
+        while (j > 0 and compareFn(elements[j - 1], elements[j]) == Ordering.After) : (j -= 1) {
             swap(elements, j, j - 1);
         }
     }
+}
+
+fn compareElements(item1: i32, item2: i32) Ordering {
+    if (item1 < item2) {
+        return Ordering.Before;
+    } else if (item1 > item2) {
+        return Ordering.After;
+    }
+    return Ordering.Equal;
 }
 
 fn swap(elements: []i32, index1: usize, index2: usize) void {
--- src/sort_test.zig
+++ src/sort_test.zig
@@ -13,3 +13,23 @@ test "should sort integer array" {
         try testing.expectEqual(expectedElements, elements);
     }
 }
+
+test "should sort integer array using custom comparison function" {
+    var elements = [_]i32{ 10, 3, 78, 34, 2 };
+    const expectedElements = [_]i32{ 78, 34, 10, 3, 2 };
+
+    inline for ([_]sorting.SortType{ .Bubble, .Selection, .Insertion }) |sortType| {
+        sorting.customSort(&elements, sortType, customCompare);
+
+        try testing.expectEqual(expectedElements, elements);
+    }
+}
+
+fn customCompare(item1: i32, item2: i32) sorting.Ordering {
+    if (item1 > item2) {
+        return sorting.Ordering.Before;
+    } else if (item1 < item2) {
+        return sorting.Ordering.After;
+    }
+    return sorting.Ordering.Equal;
+}
